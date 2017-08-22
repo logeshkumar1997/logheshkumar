8ec77251dfcb0a457e1a9404be0e4b93d01ca42a
logeshkumar1997 <30927122+logeshkumar1997@users.noreply.github.com>
2017-08-22 16:43:20 +0530

    Create sum of number.c

---
+++ sum of number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int n,a[i],sum=;
+printf(enter the values of n and values);
+scanf("%d",%n );
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i];
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+printf("sum of give %d is %d",n,sum);
+}
+
