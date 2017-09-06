2190b88dbcd3691286ab7d61b7ca0a452cabae61
logeshkumar1997 <30927122+logeshkumar1997@users.noreply.github.com>
2017-09-06 20:52:06 +0530

    Create alphabat.c

---
+++ alphabat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    char h;
+    printf("Enter the character of required ");
+    scanf("%c",&c);
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%c is an alphabet.",h);
+    else
+        printf("%c is not an alphabet.",h);
+
+    
+}
